9c8b004234dee9421f499f864f244b8ed7c142fe
lwkjob <lwkjob@gmail.com>
2015-12-09 18:31:35 +0800

    增加日志pom，修改配置文件

--- src/main/java/com/lwk/OutputService.java
+++ src/main/java/com/lwk/OutputService.java
@@ -1,15 +1,24 @@
 package com.lwk;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.io.File;
 import java.io.PrintWriter;
 import java.util.concurrent.TimeUnit;
 
 public class OutputService {
+    static  Logger logger= LoggerFactory.getLogger(OutputService.class);
+
     public static void output(String filename, int a, int b) throws Exception {
         PrintWriter out = new PrintWriter(new File(filename));
         out.write(a + ":" + b);
         TimeUnit.SECONDS.sleep(2);
         out.close();
 
+    }
+
+    public static void main(String[] args) {
+        logger.info("sfsaf");
     }
 }
